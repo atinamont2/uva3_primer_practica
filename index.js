f58ce2658bd1d1037942134732bf6ffa9b813f7b
atinamont2 <165064554+atinamont2@users.noreply.github.com>
2024-05-08 16:47:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//const nombre = 'Agustina'
+//const edad = '25'
+//const ciudad  = 'Buenos Aires'
+//
+//alert('Hola, mi nombre es ' + nombre + ' tengo '+ edad + ' años y vive en ' + ciudad)
+
+
+ const nombre = prompt('Ingrese su nombre')
+ const edad = prompt('Ingrese su edad')
+ const ciudad  = prompt('Ingrese su ciudad')
+ 
+ alert('Hola, su nombre es ' + nombre + ' tiene '+ edad + ' años y vive en ' + ciudad)
+
+const diasDeVida = 356*edad
+
+alert('Usted ha vivido ' +  diasDeVida + ' dias')
+
